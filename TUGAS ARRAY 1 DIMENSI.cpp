46f6a983949e6d8c6fe8d90e9a2f499fd489fbf6
Mochibnusaputra <96333761+Mochibnusaputra@users.noreply.github.com>
2022-02-25 15:31:08 +0700

    Add files via upload

---
+++ TUGAS ARRAY 1 DIMENSI.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int nilai[5]={65,72,50,80,95};
+	int i;
+	for (i=0;i<5;i++){
+		cout <<"Cetak Nilai Ke-"<<i+1<<" = "<<nilai[i]<<endl;
+	}
+	return 0;
+}
